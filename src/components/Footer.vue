12941dd9f392cc70a1d34ee290a2eed2f49a5399
LuizBlume <luiz.fernando.251107@gmail.com>
2023-12-24 19:35:25 -0300

    hotfix: footer em construção

---
+++ src/components/Footer.vue
@@ -0,0 +1,68 @@
+<template>
+  <footer>
+    <div class="footer">
+      <div class="footer-logo">
+        <img src="../assets/logo.png" alt="logo" />
+      </div>
+      <div class="footer-social">
+        <a href="www.linkedin.com/in/luiz-fernando-blume-jacinto-532447280" target="_blank">
+          <div class="icons-background">
+            <img src="../assets/images/linkedin-icon.png" alt="linkedin" class="icons" /></div
+        ></a>
+        <a href="https://www.instagram.com/blume.047" target="_blank">
+          <div class="icons-background">
+            <img src="../assets/images/instagram-icon.png" alt="instagram" class="icons" /></div
+        ></a>
+        <a href="https://www.github.com/LuizBlume" target="_blank">
+          <div class="icons-background">
+            <img src="../assets/images/github-icon.png" alt="github" class="icons" /></div
+        ></a>
+      </div>
+      <div class="footer__links">
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#">Terms of Use</a>
+        <a href="#">Privacy Policy</a>
+      </div>
+    </div>
+  </footer>
+</template>
+<style scoped>
+.footer {
+  background: linear-gradient(#141414 0%, #8d8d8d 100%);
+  padding: 20px;
+  backdrop-filter: blur(5px);
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.icons {
+  width: 50px;
+  height: 50px;
+}
+.footer-social {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.icons-background {
+  background-color: rgba(192, 192, 192, 0.7);
+  border-radius: 50%;
+  width: 85px;
+  height: 85px;
+  padding: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 10px;
+}
+.icons-background:hover {
+  background-color: rgba(192, 192, 192, 0.9);
+  box-shadow: 1px 1px 10px 10px #900ECD;
+  transition: 1s;
+  scale: 1.1;
+}
+</style>
+<script setup>
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,42 +1,77 @@
 <template>
-    <header>
-        <nav class="nav">
-            <img src="../assets/images/casa.png" alt="" class="icons">
-            <p class="p-nav">Home</p>
-            <img src="../assets/images/code.png" alt="" class="icons">
-            <p class="p-nav">Projects</p>
-            <img src="../assets/images/perfil.png" alt="" class="icons">
-            <p class="p-nav">About me</p>
-        </nav>
-    </header>
+  <header>
+    <nav class="nav">
+      <img src="../assets/images/casa.png" alt="" class="icons" />
+      <p class="p-nav">Home</p>
+      <img src="../assets/images/code.png" alt="" class="icons" />
+      <p class="p-nav">Projects</p>
+      <img src="../assets/images/perfil.png" alt="" class="icons" />
+      <p class="p-nav">About me</p>
+    </nav>
+  </header>
 </template>
-<script setup>
-</script>
 <style scoped>
-    header {
-        background: linear-gradient(#8d8d8d 0%, #141414 100%);
-        padding: 20px;
-        backdrop-filter: blur(5px);
-        position: sticky;
-        color: white;
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-    }
-    .icons {
-        width: 50px;
-        height: 50px;
-        margin: 0 10px 0 20px;
-    }
-    .nav {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-    }
-    .p-nav {
-        margin-right: 20px;
-        font-size: 2.3rem;
-        display: flex;
-        justify-content: center;
-    }
+header {
+  background: linear-gradient(#8d8d8d 0%, #141414 100%);
+  padding: 20px;
+  backdrop-filter: blur(5px);
+  position: sticky;
+  color: white;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+.icons {
+  width: 50px;
+  height: 50px;
+  margin: 0 10px 0 20px;
+}
+.nav {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+.p-nav {
+  margin-right: 20px;
+  font-size: 2.3rem;
+  display: flex;
+  justify-content: center;
+}
+@media (max-width: 425px) {
+  header {
+    padding: 10px;
+    justify-content: center;
+  }
+  .icons {
+    width: 40px;
+    height: 40px;
+    margin: 0 5px 0 10px;
+  }
+  .p-nav {
+    margin-right: 10px;
+    font-size: 1.5rem;
+  }
+}
+@media (min-width: 426px) and (max-width: 768px) {
+  .icons {
+    width: 40px;
+    height: 40px;
+    margin: 0 5px 0 10px;
+  }
+  .p-nav {
+    margin-right: 10px;
+    font-size: 1.5rem;
+  }
+}
+@media (min-width: 769px) and (max-width: 1024px) {
+  .icons {
+    width: 40px;
+    height: 40px;
+    margin: 0 5px 0 10px;
+  }
+  .p-nav {
+    margin-right: 10px;
+    font-size: 1.8rem;
+  }
+}
 </style>
